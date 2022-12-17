d284f2044b927c560cdd10302b48c7559db5f1ec
ChubbsBunns <troskproductions@gmail.com>
2022-12-17 18:43:40 +0800

    all scenes transitions implemented from start screen all the way till boss

--- My project/Assets/Scripts/BossScripts/BossWindUpState.cs
+++ My project/Assets/Scripts/BossScripts/BossWindUpState.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class BossWindUpState : BossState
 {
